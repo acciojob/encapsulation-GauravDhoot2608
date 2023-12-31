bd6fc2d2351cd624489389762a79247942a780d9
Gaurav Dhoot <gauravdhoot17@gmail.com>
2023-12-31 23:17:04 +0530

    Project Committed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        RWOnly rwOnly = new RWOnly();
+
+        // rwOnly.name is not accesible in this class due to encapsulation
+
+        rwOnly.setName("Gaurav");
+
+        String name = rwOnly.getName();
+    }
   
 }
